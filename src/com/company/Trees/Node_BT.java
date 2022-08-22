bc34fa66fa084df058de32f1017c55f52589dcf4
Zeeshan Desai <desaizeeshan22@gmail.com>
2022-08-21 23:15:43 -0500

    Added node for binary trees and inorder traversal(recursive and iterative)

---
+++ src/com/company/Trees/Node_BT.java
@@ -0,0 +1,14 @@
+package com.company.Trees;
+
+public class Node_BT {
+    public Node_BT lchild;
+    public Node_BT rchild;
+    public int data;
+
+    public Node_BT(int data) {
+        this.data = data;
+        this.lchild = null;
+        this.rchild = null;
+    }
+}
+
---
+++ src/com/company/Trees/main_tree.java
@@ -0,0 +1,41 @@
+package com.company.Trees;
+
+import java.util.Stack;
+
+public class main_tree {
+    public static void main(String[] args) {
+        Node_BT root = new Node_BT(10);
+        root.lchild = new Node_BT(20);
+        root.rchild = new Node_BT(30);
+        root.lchild.lchild = new Node_BT(5);
+        root.lchild.rchild = new Node_BT(25);
+        root.rchild.rchild = new Node_BT(35);
+        InorderTraversal(root);
+        System.out.println(" ");
+        iterativeInorderTraversal(root);
+    }
+
+    public static void InorderTraversal(Node_BT root) {
+        if (root == null) {
+            return;
+        }
+        InorderTraversal(root.lchild);
+        System.out.print(root.data + " ");
+        InorderTraversal(root.rchild);
+    }
+
+    public static void iterativeInorderTraversal(Node_BT root) {
+        Stack<Node_BT> st = new Stack<Node_BT>();
+        Node_BT curr = root;
+        while (!st.isEmpty() || curr != null) {
+            if (curr != null) {
+                st.push(curr);
+                curr = curr.lchild;
+            } else {
+                curr = st.pop();
+                System.out.print(curr.data +" ");
+                curr=curr.rchild;
+            }
+        }
+    }
+}
